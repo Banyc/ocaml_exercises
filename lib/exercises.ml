2b1f668ae9e2cc0f33a14e4921ddb2651e321d25
Banyc <36535895+Banyc@users.noreply.github.com>
2023-11-13 22:04:20 +0800

    feat: at, length

--- lib/exercises.ml
+++ lib/exercises.ml
@@ -20,3 +20,33 @@ let%test _ =
   (last_two ["a"; "b"; "c"; "d"]) = (Some ("c", "d"))
 let%test _ =
   (last_two ["a"]) = (None)
+
+(* N'th Element of a List *)
+let rec at ((list: 'a list), (index: int)) =
+  if index < 0 then None
+  else if index = 0 then match list with
+    | first :: _rest -> Some first
+    | _ -> None
+  else match list with
+    | _first :: rest -> at (rest, (index - 1))
+    | _ -> None
+
+let%test _ =
+  (at (["a"; "b"; "c"; "d"; "e"], 2)) = (Some "c")
+let%test _ =
+  (at (["a"], 2)) = (None)
+let%test _ =
+  (at ([], 2)) = (None)
+
+(* Length of a List *)
+let length (list: 'a list) =
+  let rec length_impl ((list: 'a list), (count: int)) = match list with
+  | _first :: rest -> length_impl (rest, count + 1)
+  | _ -> count
+  in
+  length_impl (list, 0)
+
+let%test _ =
+  (length ["a"; "b"; "c"]) = 3
+let%test _ =
+  (length []) = 0
